20c1fb040bdfd39dd064779c5a196e422c612fcc
Hellsingi <f.c.realmadrid@mail.ru>
2021-10-14 18:59:39 +0300

    feat: create TodoList component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
+/* eslint-disable react/jsx-filename-extension */
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Typography } from '@material-ui/core';
-import './styles/main.css';
 import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
-const App = () => (
-  // eslint-disable-next-line react/jsx-filename-extension
-  <div className="App">
-    <Typography component="h1" variant="h2">
-      Todos
-    </Typography>
-    <TodoForm saveTodo={console.log} />
-  </div>
-);
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+      <TodoForm saveTodo={console.log} />
+      <TodoList todos={todos} />
+    </div>
+  );
+};
 
 const rootElement = document.querySelector('#root');
 ReactDOM.render(<App />, rootElement);
