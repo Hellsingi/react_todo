9bad8ab7c1423f97e33f17ca4f3f49e2de230c7b
Hellsingi <f.c.realmadrid@mail.ru>
2021-10-15 11:08:13 +0300

    feat: add opportunity delete task

--- src/index.js
+++ src/index.js
@@ -12,8 +12,21 @@ const App = () => {
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
-      <TodoForm saveTodo={console.log} />
-      <TodoList todos={todos} />
+      <TodoForm
+        saveTodo={(todoText) => {
+          const trimmedText = todoText.trim();
+          if (trimmedText.length > 0) {
+            setTodos([...todos, trimmedText]);
+          }
+        }}
+      />
+      <TodoList
+        todos={todos}
+        deleteTodo={(todoIndex) => {
+          const newTodos = todos.filter((_, index) => index !== todoIndex);
+          setTodos(newTodos);
+        }}
+      />
     </div>
   );
 };
